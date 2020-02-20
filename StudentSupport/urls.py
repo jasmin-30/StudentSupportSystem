70c7eca9c31179c3f25b769aa89827ab3c5e3297
Jasmin Makwana <41816446+jasmin-30@users.noreply.github.com>
2020-02-20 11:43:23 +0530

    dashboard_student.html added

--- StudentSupport/urls.py
+++ StudentSupport/urls.py
@@ -3,4 +3,5 @@ from StudentSupport import views
 
 urlpatterns = [
     path('', views.HomePageView, name='home'),
+    path('student/dashboard/', views.StudentDashboard, name='student_dashbaord'),
 ]
--- StudentSupport/views.py
+++ StudentSupport/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def HomePageView(request):
     return render(request, 'index.html', context={})
+
+
+def StudentDashboard(request):
+    return render(request, 'dashboard_student.html', context={})
