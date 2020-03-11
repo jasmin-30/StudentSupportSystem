986f15694e27da59fc24aec2f4a230999e9a8f63
kd1398 <43278948+kd1398@users.noreply.github.com>
2020-03-11 06:19:53 -0700

    url for faculty dashboard is added

--- StudentSupport/urls.py
+++ StudentSupport/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.HomePageView, name='home'),
     path('register/', views.RegisterView, name='register'),
     path('student/dashboard/', views.StudentDashboard, name='student_dashbaord'),
+    path('faculty/dashboard/', views.FacultyDashboard, name='faculty_dashboard'),
 ]
