862847855b3f487cc69a461cd4ab41acdaf24b8a
kd1398 <43278948+kd1398@users.noreply.github.com>
2020-03-12 05:43:58 -0700

    urls for hod_dashboard, hod_view_detailed_feedback and hod_view_average_feedback added

--- StudentSupport/urls.py
+++ StudentSupport/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('committee/dashboard/', views.CommitteeDashboard, name='committee_dashboard'),
     path('faculty/viewdetailedfeedback/', views.FacultyViewDetailedFeedback, name='view_detailed_feedback'),
     path('faculty/viewaveragefeedback/', views.FacultyViewAverageFeedback, name='view_average_feedback'),
+    path('hod/dashboard/', views.HodDashboard, name='hod_dashboard'),
+    path('hod/viewdetailedfeedback/', views.HodViewDetailedFeedback, name='hod_view_detailed_feedback'),
+    path('hod/viewaveragefeedback/', views.HodViewAverageFeedback, name='hod_view_average_feedback'),
 ]
