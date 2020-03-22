0147627811fba343715fcfc088dd797c309023ac
kd1398 <43278948+kd1398@users.noreply.github.com>
2020-03-22 08:06:27 -0700

    url for faculty_profile.html page added

--- StudentSupport/urls.py
+++ StudentSupport/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('principal/dashboard/', views.PrincipalDashboard, name='principal_dashboard'),
     path('principal/department/', views.Department, name='department'),
     path('student/dashboard/student_profile', views.StudentProfile, name='student_profile'),
+    path('faculty/dashboard/faculty_profile', views.FacultyProfile, name='faculty_profile'),
 ]
